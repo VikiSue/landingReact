16fc0edc346104c444752d63026f72e48ab67d9f
RomanLev4yk <romanlev4yk@ukr.net>
2020-05-05 16:44:29 +0300

    Updates

---
+++ precache-manifest.464c8fb5b5004db2c89e737b79178e69.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4203b2ce5f4ef9ae797f12ef6be6155e",
+    "url": "/landingReact/index.html"
+  },
+  {
+    "revision": "c5c2845690f8f1139e5e",
+    "url": "/landingReact/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "4a0956a1a6e263fd162f",
+    "url": "/landingReact/static/css/main.cfa07c57.chunk.css"
+  },
+  {
+    "revision": "c5c2845690f8f1139e5e",
+    "url": "/landingReact/static/js/2.033f236f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/landingReact/static/js/2.033f236f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a12c53283b0e2084ef93",
+    "url": "/landingReact/static/js/3.69cb93d7.chunk.js"
+  },
+  {
+    "revision": "4a0956a1a6e263fd162f",
+    "url": "/landingReact/static/js/main.05737efd.chunk.js"
+  },
+  {
+    "revision": "93c6af7988b4d0233322",
+    "url": "/landingReact/static/js/runtime-main.e2d94efa.js"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/landingReact/static/media/banner-photo.f5443d74.jpg"
+  }
+]);
